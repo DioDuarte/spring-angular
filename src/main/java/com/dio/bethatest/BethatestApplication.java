736403b83d3407094465ebb8fc9908b18cd05423
DioDuarte <diovany1@unesc.net>
2021-06-24 20:10:51 -0300

    First Commit

---
+++ src/main/java/com/dio/bethatest/BethatestApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bethatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bethatest/BethatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bethatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
