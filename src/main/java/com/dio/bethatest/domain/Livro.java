2200b0d5bc6f309af6f3f8e7d24a4cb9df193f7c
DioDuarte <diovany1@unesc.net>
2021-06-24 21:35:00 -0300

    Commit Anotações de Classe

--- src/main/java/com/dio/bethatest/domain/Livro.java
+++ src/main/java/com/dio/bethatest/domain/Livro.java
@@ -1,14 +1,29 @@
 package com.dio.bethatest.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String Titulo;
 	private String NomeAutor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoriaId")
 	private Categoria categoria;
 
 	public Livro() {
